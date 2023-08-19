b14b40bea8edfcf43e00bf2232a726b8c7e992ff
Bandar1014 <Bandar1014@gmail.com>
2023-08-19 03:18:04 +0300

    new5

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-async function show() {
-    const res=await fetch("https://raw.githubusercontent.com/nawafalqari/azkar-api/56df51279ab6eb86dc2f6202c7de26c8948331c1/azkar.json")
-    const data= await res.json()
-    for(let i=0;i<data.length;i++){
-        console.log(data)
+const all_counters = document.querySelectorAll(".counter")
 
-       show() 
-    }
-
-}
+all_counters.forEach(counter => {
+    counter.addEventListener('click', () => {
+        let currentCount = parseInt(counter.innerHTML);
+        if (currentCount > 0) {
+            currentCount--;
+            counter.innerHTML = currentCount;
+            if (currentCount === 0) {
+                counter.classList.add('finish');
+            }
+        }
+    })
+})
