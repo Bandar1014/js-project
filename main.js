04611437f340f42c0b41adee588d60a112ddca29
Bandar1014 <Bandar1014@gmail.com>
2023-08-19 18:37:08 +0300

    new 7

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const all_counters = document.querySelectorAll(".counter")
-
 all_counters.forEach(counter => {
     counter.addEventListener('click', () => {
         let currentCount = parseInt(counter.innerHTML);
@@ -12,3 +11,22 @@ all_counters.forEach(counter => {
         }
     })
 })
+
+const searchBar=document.getElementById("search");
+searchBar=addEventListener("keyup" , e =>{
+    let currentValue= e.target.value
+    // console.log(currentValue)
+    let titles=document.querySelectorAll("h5.card-title")
+    titles.forEach(title => {
+        if(title.textContent.includes(currentValue)){
+            title.parentNode.parentNode.style.display="";
+
+        }else{
+            title.parentNode.parentNode.style.display="none"
+
+        }
+    })
+});
+
+
+
